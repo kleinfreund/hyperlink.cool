ecf84c65ac440de6c9c012975807ce077c1c03bf
kleinfreund <phrudloff@gmail.com>
2016-11-10 10:15:51 +0100

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var gutil = require('gulp-util');
+var order = require('gulp-order');
 var babel = require('gulp-babel');
 
 var paths = {
@@ -18,29 +19,6 @@ gulp.task('js', function() {
             presets: ['es2015']
         }))
         .pipe(concat('hyperlink.cool.min.js'))
-        .pipe(uglify().on('error', gutil.log))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('js-hyperlink-cool', function() {
-    return gulp.src([
-            './js/hyperlink.cool.js',
-            '!./js/hyperlink.cool.min.js'
-        ])
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(concat('hyperlink.cool.min.js'))
-        .pipe(uglify().on('error', gutil.log))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('js-fuse', function() {
-    return gulp.src([
-            './js/fuse.js',
-            '!./js/fuse.min.js'
-        ])
-        .pipe(concat('fuse.min.js'))
         .pipe(uglify().on('error', gutil.log))
         .pipe(gulp.dest('js'));
 });
