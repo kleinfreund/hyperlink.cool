a595247c77042f0249e8021534e99f313c4e9a18
kleinfreund <phrudloff@gmail.com>
2018-08-17 08:55:30 +0200

    Update to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,19 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 
-const paths = {
-    scripts: ['./js/*.js', '!./js/*.min.js']
-};
-
 gulp.task('js', function () {
-    return gulp.src([
-        './js/fuse.js',
-        './js/hyperlink.cool.js',
-        '!./js/hyperlink.cool.min.js'
-    ])
-        .pipe(concat('hyperlink.cool.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('js'));
+  return gulp.src([
+    './js/fuse.js',
+    './js/hyperlink.cool.js',
+    '!./js/hyperlink.cool.min.js'
+  ])
+    .pipe(concat('hyperlink.cool.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./js/**/*.js'], ['js']);
+  gulp.watch(['./js/**/*.js'], ['js']);
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', gulp.parallel('js', 'watch'));
